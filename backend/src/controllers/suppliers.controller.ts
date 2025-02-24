d5de54774006982bfb9734530a5577bf07625c54
Diego <diegopablo012@gmail.com>
2025-02-23 19:17:36 -0600

    feat(suppliers.controllers): create the first controller for get all suppliers

---
+++ backend/src/controllers/suppliers.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { session } from "../config/db";
+
+export const getAllSuppliers = async (req: Request, res: Response) => {
+  try {
+    const result = await session.run(`MATCH (s:Supplier) RETURN s`);
+    
+    const suppliers = result.records.map(record => record.get("s").properties);
+
+    res.json(suppliers);
+  } catch (error) {
+    console.error("Error obteniendo proveedores:", error);
+    res.status(500).json({ error: "Error obteniendo proveedores" });
+  }
+};
