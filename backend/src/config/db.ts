8aef3ea30d1a29137e28e5d6a0d191258802789f
Diego <diegopablo012@gmail.com>
2025-02-23 19:09:33 -0600

    feat(db.ts): create driver for connect to the db

---
+++ backend/src/config/db.ts
@@ -0,0 +1,13 @@
+import neo4j from "neo4j-driver";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const driver = neo4j.driver(
+  process.env.NEO4J_URI as string,
+  neo4j.auth.basic(process.env.NEO4J_USER as string, process.env.NEO4J_PASSWORD as string)
+);
+
+const session = driver.session();
+
+export { driver, session };
