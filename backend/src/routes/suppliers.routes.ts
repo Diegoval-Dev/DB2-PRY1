0ef4fa3c2740a12afff3401a74da6f2453079ee3
Diego <diegopablo012@gmail.com>
2025-02-23 19:13:59 -0600

    feat(suppliers.routes.ts): first route

--- backend/src/routes/suppliers.routes.ts
+++ backend/src/routes/suppliers.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { getAllSuppliers } from "../controllers/suppliers.controller";
 
 const router = Router();
 
-
+router.get("/", getAllSuppliers);
 
 export default router;
