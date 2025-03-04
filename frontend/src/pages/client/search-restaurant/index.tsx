22d25aa2ecaec8041f9d2ce9fe7b057a2fb04262
Gerson Ramirez <ram22281@uvg.edu.gt>
2025-03-03 22:06:33 -0600

    refactor: add implementation of searching by restaurant

--- frontend/src/pages/client/search-restaurant/index.tsx
+++ frontend/src/pages/client/search-restaurant/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Button from '@components/client/Button'
-import Input from '@components/client/Input'
-import styles from './styles.module.css' 
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@components/client/Button';
+import Input from '@components/client/Input';
+import styles from './styles.module.css';
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 const Home = () => {
+  const [suppliers, setSuppliers] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${API_URL}/location`)
+      .then(response => setSuppliers(response.data))
+      .catch(error => console.error("Error cargando proveedores:", error));
+  }, []);
+
   return (
     <div className={styles.pageContainer}>
       <header className={styles.header}>
@@ -12,7 +23,11 @@ const Home = () => {
       </header>
       <main className={styles.main}>
         <div className={styles.card}>
-          <Input className={styles.input} placeholder="Ingrese el nombre del restaurante" />
+          <Input 
+            className={styles.input} 
+            placeholder="Ingrese el nombre del restaurante"
+            suggestions={suppliers} 
+          />
           <div className={styles.buttonGroup}>
             <Link to="/user/create-order">
               <Button>Ingresar orden</Button>
@@ -21,7 +36,7 @@ const Home = () => {
         </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
